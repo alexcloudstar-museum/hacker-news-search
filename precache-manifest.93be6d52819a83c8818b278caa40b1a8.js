656c98ad71456f6ab62980d524748090e3a7b531
alexandrurdev <alexcloudstar@gmail.com>
2020-03-18 20:24:38 +0200

    Updates

---
+++ precache-manifest.93be6d52819a83c8818b278caa40b1a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f73617f2bcf4aa5ab201bc22094372",
+    "url": "/Hacker-News-Search/index.html"
+  },
+  {
+    "revision": "2cd1fa2b0738641076cc",
+    "url": "/Hacker-News-Search/static/css/2.389269c8.chunk.css"
+  },
+  {
+    "revision": "7bb0c10fd58f24bcf1c1",
+    "url": "/Hacker-News-Search/static/css/main.4297eab3.chunk.css"
+  },
+  {
+    "revision": "2cd1fa2b0738641076cc",
+    "url": "/Hacker-News-Search/static/js/2.a3d06b6b.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/Hacker-News-Search/static/js/2.a3d06b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb0c10fd58f24bcf1c1",
+    "url": "/Hacker-News-Search/static/js/main.93f2f995.chunk.js"
+  },
+  {
+    "revision": "18c53f602264191c34e9",
+    "url": "/Hacker-News-Search/static/js/runtime-main.38c66ce7.js"
+  }
+]);
